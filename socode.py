33c7352246a6e9cb6f06ce615553ad0a4e427e79
Srivatsa Viswanathan <sri.umd@gmail.com>
2012-10-14 18:49:26 -0400

    First

---
+++ socode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Welcome to soCode, the social coding experiment.
+# Here are the rules! 
+#       - Add a funtion with your github name as its name.
+#       - Your function can do anything and everything, as long as it
+#         finishes in a reasonable amount of time (no infinite loops please)
+#       - Once your function is written, you can call it from anywhere else in the 
+#         already existing exectution chain.
+#       - Never entirely delete anyone else's function, edit it if needed.
+#       - lefin() should be the last executing function, always
+
+# Lets see if we can make something meaningful out of this.
+
+def sricola():
+    print "Welcome to soCode!!"
+
+def lefin():
+    print "Goodbye Social World!"
+    
+if __name__ == "__main__":
+    sricola()
+    lefin()
