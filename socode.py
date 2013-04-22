939ac47ce0605a40157ef5298ae59a7eabeed5f7
myeaple <mdy7643@g.rit.edu>
2013-04-22 08:41:57 -0400

    Cleaned up function and made it tweet about socode.

--- socode.py
+++ socode.py
@@ -556,6 +556,22 @@ def dpayne():
         print 'There was an error'
     
     return False
+	
+def myeaple():
+	new = 2 # Open a new tab, if possible
+	
+	num_fn = len([obj for name, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isfunction(obj)])
+	
+	text = "There%20are%20currently%20" + (str)(num_fn) + "%20functions%20in%20socode.py"
+	hashtags = "socode"
+	socode_url = "https%3A%2F%2Fgithub.com%2Fsricola%2Fsocode"
+	
+	print "Number of functions in socode.py = " + (str)(num_fn)
+	
+	# Tweet the number of functions in socode.py
+	url = "https://twitter.com/intent/tweet?url=" + socode_url + "&text=" + text + "&hashtags=" + hashtags
+	
+	webbrowser.open(url, new=new)
   
 if __name__ == "__main__":
     starefossen()
@@ -615,6 +631,7 @@ if __name__ == "__main__":
     windspy()
     samlabs821()
     alisnic(random.randint(1, 1024))
+	myeaple()
     
     # as per instructed, please leave this as the final function
     lafin()
