37de99054bc4baff9c51b4a0edcb377f601563e7
Sam Schooler <schooler.sam@gmail.com>
2013-04-21 21:35:04 -0500

    Update socode.py

--- socode.py
+++ socode.py
@@ -8,24 +8,76 @@
 #       - Once your function is written, you can call it from anywhere else in the 
 #         already existing exectution chain.
 #       - Never entirely delete anyone else's function, edit it if needed.
-#       - lafin() should be the last executing function, always
+#       - lefin() should be the last executing function, always
 
 # Lets see if we can make something meaningful out of this.
 
-import datetime
-
+import json
+import requests
+import random
+import getpass
+    
 def sricola():
     print "Welcome to soCode!!"
+    
+def kisom():
+    ip = requests.get('http://ifconfig.me/ip').content
+    geoip = json.loads(requests.get('http://freegeoip.net/json/').content)['country_name']
+    print 'there once was a coder from', geoip
+    
+def evinugur():
+    print 'there is a ', (random.random()*100), "% chance that something cool will come from this..."
+    
+def lafin(): # Please speak proper french :)
+    print "Goodbye Social World!"
+    print "\nStarted with <3 in Brooklyn, NY\n"
+
+def prezjordan():
+    h,s='.;*&8#',''
+    for q in range(801):
+        if q%40==0:print s;s=''
+        i,k=0,0
+        while(abs(k)<2*(i<15)):k,i=k**2+complex(q%40*.075-2,q/40*-.1+1),i+1
+        s+=h[i/3]*2
+    
+def JesseAldridge():
+    def wrap(f):
+      def new_f():
+        for i in range(1):
+          f()
+      return new_f
+
+    g = globals()
+    for k, v in g.iteritems():
+      if callable(v) and k != 'JesseAldridge':
+        g[k] = wrap(v)
+    
+def shuhaowu(): # Call me last! :D
+    l = locals()
+    for f in l.keys():
+        if not (f.startswith("_") or f == "shuhaowu"):
+            del l[f] # ^_^
+
+    print "Goodbye, cruel world"
+    
+def agoebel():
+    print "America!"
+
+def tcr():
+    print "You know we love you, ", getpass.getuser(), "."
     
 def quaz3l():
     print "Today is " + str(datetime.date.today()) + ", quaz3l was here on 2013-04-21"
     if(str(datetime.date.today()) == '2013-04-21'): 
         print "Hey! thats today!"
-
-def lafin():
-    print "Goodbye Social World!"
     
 if __name__ == "__main__":
+    evinugur()
+    JesseAldridge()
     sricola()
+    kisom()
+    prezjordan()
+    tcr()
+    agoebel()
     quaz3l()
     lafin()
