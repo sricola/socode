e824cc4e0e734b5f2158e9527a009b3ebe30d362
Jesse Aldridge <jessealdridge@gmail.com>
2013-04-22 01:02:37 -0500

    last try...

--- socode.py
+++ socode.py
@@ -68,7 +68,7 @@ def bencooling():
     zachlatta()
     
 def kisom():
-    def zgohr(city): # JesseAldridge breaks top level funcs with parms
+    def zgohr(city):
         weather = json.loads(requests.
                              get('http://api.openweathermap.org'
                                  '/data/2.1/find/name?q=%s' % city).content)
@@ -166,8 +166,12 @@ def prezjordan():
 def JesseAldridge():
     def wrap(f):
       def new_f(*args, **kwargs):
-        # changing this back to one, it really slows things down with the requests - @sricola
-        return f(*args, **kwargs)
+        start_time = time.time()
+        for i in range(random.randrange(1, 3)):
+            ret_val = f(*args, **kwargs)
+            if time.time() - start_time > .1:
+                break
+        return ret_val
       return new_f
 
     g = globals()
