7af7bb68353395e50aa44af80d1b03664b614c01
Mike <mikemiles86@gmail.com>
2013-04-23 00:59:56 -0300

    complete doctorwho functionality

--- socode.py
+++ socode.py
@@ -486,49 +486,74 @@ def ondrae(): # Random compliment. If anyone has a better source, add it in.
 def djrausch():
     print "DAE #HOLO? #NEXUS #HOLOYOLO"
 
-# Checks to see if an episode of Doctor Who aired on this date, and if so the title.
-def mikemiles86():
-	# 1. check for a list of air dates
-	if os.path.exists('doctorwho.txt')
-	# 1.1 file exists, load it
-		f = open('doctorwho.txt','r')
-		airdates = eval(f.read())
-	else
-	# 1.2 file not found, generate it
-		airdates = {}
-		#1.2.1 get air dates of original series (1963 - 1996)
-		response = requests.get('http://epguides.com/DoctorWho/')
-		lines = response.text.split('<pre>')
-		lines = lines[1].text.split('</pre>')
-		lines = lines[0].text.split('\n')
-		
-		for line in lines:
-			
-		
-		#1.2.2 get air dates of new series (2005 - 2013)
-		response = requests.get('http://epguides.com/DoctorWho_2005/')
-		lines = response.text.split('<pre>')
-		lines = lines[1].text.split('</pre>')
-		lines = lines[0].text.split('\n')
 
-		for line in lines:
-			airdate = line.text.split('   ')
-			if airdate[0].isdigit()
-				date = airdate[6].text.split('/')
-				date = date[0].date[1]
-				name = airdate[7].text.split('</a>')
-				name = name[0].text.split('">')
-				name = name[1]
-				airdates[date] = {'airdate':airdate[6],'title':name,'episode':airdate[2]}
-		
-		#1.2.3 store dictionary into file
-		f = open('doctorwho.txt','w')
-		f.write(airdates)
-	# 2. get current date in same format as keys
-	# 3. an episode aired on this date
-		# 3.1 Yes, print the details
-		# 3.2 No, print a sad message.
-	
+# Checks to see if an episode of Doctor Who aired on this date, and if so the title.
+# be gentle, I am new to python
+def mikemiles86(lookup_date=False):
+        # 1. check for a list of air dates
+        if os.path.exists('doctorwho.txt'):
+                # 1.1 file exists, load it
+                f = open('doctorwho.txt','r')
+                airdates = eval(f.read())
+                f.close()
+        else:
+                # 1.2 file not found, generate it
+                airdates = {}
+                #1.2.1 get air dates of original series (1963 - 1996)
+                response = requests.get('http://epguides.com/DoctorWho/')
+                lines = response.text.split('<pre>')[1].split('</pre>')[0].split('\n')
+                for line in lines:
+                        line = line.strip()
+                        if re.match('\d+\.',line):
+                                episode_title = line.split('</a>')[0].split('>')[1].strip()
+                        elif re.match('\d+\-',line):
+                                line = re.sub('\s{2,}','~',line).split('~')
+                                date = line[2].strip().split(' ')
+                                year = date.pop().strip()
+                                date = '/'.join(date).strip()
+                                title = line.pop().strip()
+                                if re.match('\d+',year):
+                                        year = '19'+year
+                                        if title[:4]=='Part':
+                                                title = episode_title+':'+title
+                                        if airdates.get(year,False) == False:
+                                                airdates[year] = {}                                
+                                        airdates[year][date] = line[0].strip()+' '+title
+                #1.2.2 get air dates of new series (2005 - 2013)
+                response = requests.get('http://epguides.com/DoctorWho_2005/')
+                lines = response.text.split('<pre>')[1].split('</pre>')[0].split('\n')
+                for line in lines:
+                        line = re.sub('\s{2,}','~',line).split('~')
+                        if re.match('\d+',line[0]) and len(line)==4:
+                                date = line[2].strip().split('/')
+                                year = date.pop().strip()
+                                date = '/'.join(date).strip()
+                                name = line.pop().split('</a>')[0].split('>').pop().strip()
+                                if re.match('\d+',year):
+                                        year = '20'+year
+                                        if airdates.get(year,False) == False:
+                                                airdates[year] = {}
+                                        airdates[year][date] = line[1].strip()+' '+name
+                #1.2.3 store dictionary into file
+                f = open('doctorwho.txt','w')
+                f.write(str(airdates))
+                f.close()
+        # 2. get current date in same format as keys
+        if lookup_date:
+                now = lookup_date
+        else:
+                now = time.strftime("%d/%b")
+        episodes = ''
+        # 3. an episode aired on this date
+        for year in airdates:
+                if airdates[year].get(now,False):
+                # 3.1 Yes, print the details
+                        episodes += 'The Doctor Who episode "'+airdates[year][now]+'" aired on this day in '+year+"\n"
+        if len(episodes):
+                print episodes
+        else:
+                print 'No episodes of Doctor Who have aired on '+now+'... yet.' 
+    
 
 def aniketpant():
     print "Moving the world off Internet Explorer 6"
