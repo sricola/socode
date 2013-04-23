b0832c4e41aa8f003e1e1173b612d98b29a29162
nckrdrgz <nick.rodriguez25@gmail.com>
2013-04-23 12:31:07 -0300

    Update socode.py

--- socode.py
+++ socode.py
@@ -918,6 +918,9 @@ def shazeline(name):
 
 def samlabs821():
     print "vesselam"
+    
+def nckrdrgz(month):
+	print "Holla, it's almost " + month
 
 def matiasinsaurralde():
     print "hello, this is skynet"
@@ -1269,6 +1272,7 @@ if __name__ == "__main__":
     cyclo()
     binary132()
     MikeGrace()
+    nckrdrgz('May')
     sfrnld()
     yuvadm()
     kghose()
