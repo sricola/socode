5a7fc13a5e7835c9bcdccaa2cbc2633aa03ff3af
Eric The Robot <erictherobot@gmail.com>
2013-04-23 13:44:30 -0400

    adding my username in the bottom section

--- socode.py
+++ socode.py
@@ -1232,6 +1232,7 @@ if __name__ == "__main__":
     antonaut() # DOUBLE-RAINBOW! Soo cool
     seripap("deleting ",1,"......")
     rjwebb(5)
+    erictherobot()
     sikado()
     dlad()
     starefossen()
